8ce1b7853517a87ddc1d71538f9d22bbadce9ec4
ali saghir <alisaghir543@gmail.com>
2024-11-28 00:41:34 +0400

    some touches

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
-import reef from './assets/algedra.avif'
+import dubai from './assets/interio2.png'
 import Typewriter from 'typewriter-effect';
 
 
@@ -10,13 +10,13 @@ const Home:React.FC = () => {
   return (
     <div className="relative bg-gradient-to-r h-screen text-white overflow-hidden">
     <div className="absolute inset-0">
-      <Image src={reef} alt="Background Image" className="object-cover object-center w-full h-full" />
+      <Image src={dubai} alt="Background Image" className="object-cover object-center w-full h-full" />
       <div className="absolute inset-0 bg-black opacity-50"></div>
     </div>
     
     <div className="relative z-10 flex flex-col justify-center items-center h-full text-center mx-2">
       <h1 className="text-5xl font-bold leading-tight mb-4 custom-font ">Welcome to <span className='text-customText '>High Five</span>  Properties</h1>
-      <p className="text-lg text-gray-300 mb-8 font-mono">
+      <div className="text-lg text-gray-300 mb-8 font-mono">
           <Typewriter
             options={{
               strings: [
@@ -31,7 +31,7 @@ const Home:React.FC = () => {
               delay: 75,
             }}
           />
-        </p>
+        </div>
       <a href="#Form" className="bg-customText font-mono text-white hover:bg-white hover:text-black py-2 px-6 rounded-md text-md md:text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">Register Your Intereset</a>
     </div>
   </div>
