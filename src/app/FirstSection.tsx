990602f40664bf3a11c2939b7f29da4a752cf28d
ali saghir <alisaghir543@gmail.com>
2024-11-27 18:53:42 +0400

    finsiehd first section

--- src/app/FirstSection.tsx
+++ src/app/FirstSection.tsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import Image from 'next/image'
+import reef from './assets/algedra.avif'
 
 const FirstSection:React.FC = () => {
   return (
-    <div>FirstSection</div>
+    <div className="bg-white dark:bg-gray-800 h-screen py-6 sm:py-8 lg:py-12">
+    <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
+            <div className="flex items-center gap-12">
+                <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">Our Property Gallery</h2>
+
+                <p className="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block">
+                Explore our curated selection of properties across different categories. Whether you are looking for your dream home or commercial space, we have something for every need.
+                </p>
+            </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+            <a href="#"
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                <Image src={reef} loading="lazy" alt="Photo by Minh Pham" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                </div>
+
+                <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Studio</span>
+            </a>
+
+            <a href="#"
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                <Image src={reef} loading="lazy" alt="Photo by Magicle" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                </div>
+
+                <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">1 Bedroom</span>
+            </a>
+
+            <a href="#"
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
+                <Image src={reef} loading="lazy" alt="Photo by Martin Sanchez" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                </div>
+
+                <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">2 Bedrooms</span>
+            </a>
+
+            <a href="#"
+                className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
+                <Image src={reef} loading="lazy" alt="Photo by Lorenzo Herrera" className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+
+                <div
+                    className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                </div>
+
+                <span className="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Town House</span>
+            </a>
+
+        </div>
+        
+    </div>
+</div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HomeComp from "./Home";
+import FirstSection from "./FirstSection";
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <HomeComp />
+    <FirstSection />
     </>
   );
 }
