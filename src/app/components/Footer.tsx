ebcf89cd7ba1882801711ae5befd6e4b7fd4a462
ali saghir <alisaghir543@gmail.com>
2024-11-28 15:06:14 +0400

    changed the logo and edited the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.svg'
 
 const Footer:React.FC = () => {
   return (
-    <footer className="bg-gray font-mono">
+    <footer className="bg-customBg font-mono">
   <div className="container px-4 mx-auto">
     <div className="pt-24 pb-11 mx-auto max-w-4xl"><a className="block md:mx-auto mb-5 max-w-max" href="#">
         <Image src={logo} alt='logo' className='p-5' />
@@ -13,7 +13,7 @@ const Footer:React.FC = () => {
   </div>
   <div className="border-b border-gray-100"></div>
   <div className="container px-4 mx-auto">
-    <p className="py-10 md:pb-20 text-md text-gray-400 font-medium text-center">
+    <p className="py-10 md:pb-20 text-md text-white font-medium text-center">
       © 2024 High Five Properties.
       All rights reserved.
     </p>
