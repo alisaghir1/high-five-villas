95efd0e283df0133c3f4dd5a9e146ddb41909dcf
ali saghir <alisaghir543@gmail.com>
2024-12-16 16:00:58 +0400

    added a script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,7 +16,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Dubai Luxury Villas | Buy, Rent, and Invest in Real Estate",
-  description: 'Discover your perfect villa with Hi5 Villas, your trusted real estate partner. Explore a wide range of luxurious villas, designed to meet your lifestyle. Whether buying, renting, or selling, we make your villa journey seamless and stress-free.',
+  description:
+    "Discover your perfect villa with Hi5 Villas, your trusted real estate partner. Explore a wide range of luxurious villas, designed to meet your lifestyle. Whether buying, renting, or selling, we make your villa journey seamless and stress-free.",
   icons: {
     icon: "/logo.svg", // Replace with the path to your logo image in the public folder
   },
@@ -28,6 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-WVVNJXMF6T"
+          async
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-WVVNJXMF6T');
+  `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
